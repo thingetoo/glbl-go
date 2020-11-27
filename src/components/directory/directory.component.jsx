c823f7b7c8a488d94a108f322e9fc664e2036500
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2020-11-27 13:51:35 -0500

    updated hats menu item to allow routing between pages

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,29 +10,34 @@ class Directory extends React.Component {
                 {
                     title: 'tops',
                     imageUrl: 'https://images.pexels.com/photos/102129/pexels-photo-102129.jpeg',
-                    id: 1
+                    id: 1,
+                    linkUrl: ''
                 },
                 {
                     title: 'hats',
                     imageUrl: 'https://ae01.alicdn.com/kf/HTB1BCVaFv5TBuNjSspmq6yDRVXaV/LDSLYJR-Flower-print-Bucket-Hat-Fisherman-Hat-outdoor-travel-hat-Sun-Cap-Hats-for-Men-and.jpg_Q90.jpg_.webp',
-                    id: 2
+                    id: 2,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'masks',
                     imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/advancelocal/CJS3YQOBEVANBJVSVPZKSWN5WY.jpeg',
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
                     title: 'womens',
                     imageUrl: 'https://lovelifebefit.com/wp-content/uploads/2020/09/best-down-jackets-for-women-2021-1024x576.jpg',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://cdn.shopify.com/s/files/1/1584/8213/files/Wuxly_Movement_Social_Sharing_01_7df56edd-f2ec-49d7-b680-b60ec7353856.jpg?v=1605551684',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 },
             ]
         }
@@ -40,9 +45,9 @@ class Directory extends React.Component {
     render() {
         return(
     <div className='directory-menu'>
-            {this.state.sections.map(({title, imageUrl, id, size}) => (
-                <MenuItem title={title} imageUrl={imageUrl} key={id} size={size}/>
-            ))}
+        {this.state.sections.map(({id, ...otherSectionProps}) => (    //because id !== {id} we don't include id
+            <MenuItem key={id} {...otherSectionProps}/>
+        ))}
     </div>
         )
     }
